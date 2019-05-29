135acb539e4e3d1722c25db56450f073b3fa6e56
hong <785131182@qq.com>
2019-05-29 15:42:08 +0800

    add query param slice

--- pkg/e/e_code.go
+++ pkg/e/e_code.go
@@ -7,6 +7,16 @@ var (
 	ErrorNoOpenId   = errors.New("check param: openid")
 	ErrorNoToken    = errors.New("check param: access_token")
 	ErrorInitClient = errors.New("call NewClientWithParam first")
+
+	//统一下单必须参数
+	UnifiedOrderMustParam = []string{"appid", "mch_id", "nonce_str", "sign", "body", "out_trade_no", "total_fee", "spbill_create_ip", "notify_url", "trade_type"}
+	//统一下单可选参数
+	UnifiedOrderOptionalParam = []string{"device_info", "sign_type", "detail", "attach", "fee_type", "time_start", "time_expire", "goods_tag", "limit_pay", "receipt"}
+
+	//订单查询必需参数
+	QueryOrderMustParam = []string{"appid", "mch_id", "nonce_str", "sign"}
+	//订单查询可选参数
+	QueryOrderOptionalParam = []string{"sign_type"}
 )
 
 const (
---
+++ we_queryOrder.go
@@ -0,0 +1,8 @@
+package wechat_sdk
+
+/*
+   @Create by GoLand
+   @Author: hong
+   @Time: 2019-05-29 15:24
+   @File: we_queryOrder.go
+*/
