c0c6a0b2bc8f41c210089a20f82a79a9d938287e
hong <785131182@qq.com>
2019-12-08 01:17:52 +0800

    增加判断并创建目录的函数

--- pkg/util/tool.go
+++ pkg/util/tool.go
@@ -1,6 +1,9 @@
 package util
 
-import "reflect"
+import (
+	"os"
+	"reflect"
+)
 
 //判断array中是否有元素value
 func HaveInArray(array interface{}, value interface{}) bool {
@@ -30,4 +33,17 @@ func Interface2Map(data interface{}) map[string]interface{} {
 		}
 	}
 	return result
+}
+
+//判断目录是否存在，如果不存在，则新建一个目录
+func MakeNewPath(targetPath string) error {
+	if _, err := os.Stat(targetPath); err != nil {
+		if !os.IsExist(err) {
+			//创建目录
+			if mErr := os.MkdirAll(targetPath, os.ModePerm); mErr != nil {
+				return mErr
+			}
+		}
+	}
+	return nil
 }
