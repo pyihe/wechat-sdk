7b5b5718aa99fcb92ea5f14f9f8e5d5a7af212f5
hong <785131182@qq.com>
2019-12-09 12:03:31 +0800

    fix:增加支付方式常量

--- pkg/e/consts.go
+++ pkg/e/consts.go
@@ -5,4 +5,9 @@ const (
 	SignType256        = "HMAC-SHA256"
 	UnifiedOrderApiUrl = "https://api.mch.weixin.qq.com/pay/unifiedorder"
 	QueryOrderApiUrl   = "https://api.mch.weixin.qq.com/pay/orderquery"
+
+	H5     = "MWEB"   //H5支付
+	App    = "APP"    //APP支付
+	JSAPI  = "JSAPI"  //JSAPI支付
+	Native = "NATIVE" //Native(扫码)支付
 )
