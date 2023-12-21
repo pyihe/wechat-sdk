86ecf30f5076440f2dbf769437a396d543c2f0a8
pyihe <785131182@qq.com>
2023-12-21 16:00:48 +0800

    fix

--- pkg/bytes.go
+++ pkg/bytes.go
@@ -1,5 +1,11 @@
 package pkg
 
+import "unsafe"
+
+func BytesToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
 func BytesContain(ele byte, b []byte) bool {
 	for _, v := range b {
 		if v == ele {
